5732c3c63defd19bcc1cee42ca86311fbf83a40b
amaity2 <ajay_maity@optum.com>
2019-04-13 21:44:17 +0100

    fixed code quality errors

--- eda_viz/exception.py
+++ eda_viz/exception.py
@@ -3,14 +3,14 @@
 
 class TypeNotSupportedError(Exception):
     """Raised when a non-acceptable data is passed."""
-    pass
+    pass  # pylint: disable=W0107
 
 
 class NonNumericTypeNotSupportedError(TypeNotSupportedError):
     """Raised when a non-numeric data is passed."""
-    pass
+    pass  # pylint: disable=W0107
 
 
 class InvalidDataError(Exception):
     """Raised when an invalid data is passed."""
-    pass
+    pass  # pylint: disable=W0107
